99d30cc126b84b78a92e846460d7320e39f43d19
Mohamed Loksha <mohammedloksha7@gmail.com>
2026-08-10 16:35:42 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char, int> ms, mt;
+        for (const auto& c : s) ms[c]++;
+        for (const auto& c : t) mt[c]++;
+        if (ms == mt) return true;
+        return false;
+    }
+};

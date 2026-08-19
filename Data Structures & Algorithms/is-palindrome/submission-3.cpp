7b6b399c398346f6f6cea160b39412ce3fec6647
Mohamed Loksha <mohammedloksha7@gmail.com>
2026-08-20 00:10:47 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    string str;
+    for (const auto& ch : s)
+        if (isalnum(ch)) str += tolower(ch);
+
+    const int length = str.length();
+    if (length < 2) return true;
+    for (int i = 0, j = length-1-i; i < j; i++, j--)
+        if (str[i] != str[j]) return false;
+
+    return true;
+}
+};

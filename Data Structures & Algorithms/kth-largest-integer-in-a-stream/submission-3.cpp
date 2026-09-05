d6c0fbb86251e26b95b5072c6fd3dd419b94c83f
Mohamed Loksha <mohammedloksha7@gmail.com>
2026-09-05 18:00:15 +0300

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.cpp
@@ -0,0 +1,18 @@
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<>> pq;
+    int target = 1;
+public:
+    KthLargest(int k, vector<int>& nums): target(k) {
+        for (const auto& n : nums) {
+            pq.emplace(n);
+            if (pq.size() > target) pq.pop();
+        }
+    }
+
+    int add(int val) {
+        pq.emplace(val);
+        if (pq.size() > target) pq.pop();
+        return pq.top();
+    }
+};

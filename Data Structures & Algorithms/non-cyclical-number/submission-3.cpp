66f715757285550c47cbed0b2ef28ba0b731f927
Mohamed Loksha <mohammedloksha7@gmail.com>
2026-09-08 22:33:18 +0300

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int sumSQ{};
+    void calculating(int& n) {
+        sumSQ = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sumSQ += digit * digit;
+            n /= 10;
+        }
+        n = sumSQ;
+    }
+public:
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        calculating(n);
+        while (n != 1){
+            calculating(n);
+            if (set.count(sumSQ)) return false;
+            set.emplace(sumSQ);
+        }
+        return true;
+    }
+};
